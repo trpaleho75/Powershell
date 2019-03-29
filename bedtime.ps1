3de6504bdce95cd59140ffc40b04aeb3cb5a7979
trpaleho75 <travisholt@hotmail.com>
2019-03-28 22:00:53 -0700

    Add start-bedtime.ps1

---
+++ bedtime.ps1
@@ -0,0 +1,62 @@
+
+
+function suspend-computer
+{
+	# Set power state
+	$powerState = [System.Windows.Forms.PowerState]::Suspend;
+	$force = $true;
+	$disableWake = $false;
+	[System.Windows.Forms.Application]::SetSuspendState($powerState, $force, $disableWake);
+}
+
+# Get the day of the week
+$dayOfTheWeek = (get-date).DayOfWeek;
+$hourOfTheDay = (get-date).hour;
+
+# If after 9 on weekdays go to sleep
+if ($hourOfTheDay -ge 21)
+{
+	write-host $dayOfTheWeek;
+	switch($dayOfTheWeek)
+	{
+		"Sunday"
+		{
+			suspend-computer;
+			break
+		}
+		
+		"Monday"
+		{
+			suspend-computer;
+			break
+		}
+		
+		"Tuesday"
+		{
+			suspend-computer;
+			break
+		}
+		
+		"Wednesday"
+		{
+			suspend-computer;
+			break
+		}
+		
+		"Thursday"
+		{
+			suspend-computer;
+			break
+		}
+		
+		"Friday"
+		{
+			break
+		}
+		
+		"Saturday"
+		{
+			break
+		}
+	}
+}
